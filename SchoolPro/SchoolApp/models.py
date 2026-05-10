c0c91f254e7855c85c9c61f017c31bcbb4913592
amitsaini95 <amitsaini201295@gmail.com>
2026-05-10 08:59:49 +0530

    create School Profile model

--- SchoolPro/SchoolApp/models.py
+++ SchoolPro/SchoolApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class SchoolProfileModel(models.Model):
+    admin=models.ForeignKey(User, on_delete=models.CASCADE)
+    name=models.CharField(max_length=150)
+    code=models.IntegerField()
+    SchoolEmail=models.EmailField(max_length=100)
+    SchoolPhoneNo=models.IntegerField()
+    SchoolAddress=models.TextField()
+    SchoolEmail=models.EmailField(max_length=100)
+    PrincipleName=models.CharField(max_length=100)
+    PrinciplePhoneNo=models.IntegerField()
+    PrincipleEmail=models.EmailField(max_length=100)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+  
+
+    def __str__(self):
+        return self.name
+
