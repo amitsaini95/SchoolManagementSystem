bae892cec1336aa47c6ce8c69edb8d04c569859b
amitsaini95 <amitsaini201295@gmail.com>
2026-05-10 09:35:26 +0530

    successfully user login

--- SchoolPro/BaseApp/urls.py
+++ SchoolPro/BaseApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .import views
 urlpatterns=[
-    path('',views.HomeView,name="Home")
+    path('',views.HomeView,name="Home"),
+    path('login',views.LoginView,name="Login")
 ]
--- SchoolPro/BaseApp/views.py
+++ SchoolPro/BaseApp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-
+from .forms import LoginForm
+from django.contrib.auth  import authenticate, login,logout 
 # Create your views here.
+from django.http import HttpResponseRedirect,HttpResponse
 def HomeView(request):
     return render(request,"base.html")
+def LoginView(request):
+    if request.method == "POST":
+        form=LoginForm(request=request,data=request.POST)
+        if form.is_valid():
+            uname = form.cleaned_data['username'] 
+            upass = form.cleaned_data['password']
+            user = authenticate(username=uname,password=upass)
+            if user is  not None: 
+                login(request,user)
+        
+                return HttpResponseRedirect('/')
+    else:   
+            form=LoginForm()
+    return  render(request,'login.html',{'form':form})
