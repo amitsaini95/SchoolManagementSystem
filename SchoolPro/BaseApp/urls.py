26057371c6c318f0b861b6996ce8375ae6c7bc56
amitsaini95 <amitsaini201295@gmail.com>
2026-05-11 16:11:46 +0530

    solve error

--- SchoolPro/BaseApp/urls.py
+++ SchoolPro/BaseApp/urls.py
@@ -6,11 +6,8 @@ urlpatterns=[
     path('login',views.LoginView,name="Login"),
     path('showSignList',views.showsignbuttonList,name="showsignbuttonList"),
     path('schoolSignUp',views.schoolSignUpView,name="SchoolSignUp"),
-<<<<<<< HEAD
-    path('studentSignUp',views.studentSignUpView,name="StudentSignUp"),
     path('Logout',views.LogoutView,name="Logout")
-=======
     path('studentSignUp',views.studentSignUpView,name="StudentSignUp")
->>>>>>> b2c084602b28acf29cfc940bb484ffbadc2d8282
+
 
 ]
